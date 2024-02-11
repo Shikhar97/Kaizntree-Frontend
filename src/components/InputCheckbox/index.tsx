3db3292491d9f3f7cb448260f08c60b28ee23e0a
Shikhar Gupta <sgupt330@asu.edu>
2024-02-11 13:38:17 -0700

    Solved Bug 2

--- src/components/InputCheckbox/index.tsx
+++ src/components/InputCheckbox/index.tsx
@@ -3,24 +3,22 @@ import { useRef } from "react"
 import { InputCheckboxComponent } from "./types"
 
 export const InputCheckbox: InputCheckboxComponent = ({ id, checked = false, disabled, onChange }) => {
-  const { current: inputId } = useRef(`KaizntreeInputCheckbox-${id}`)
-
-  return (
-    <div className="KaizntreeInputCheckbox--container" data-testid={inputId}>
-      <label
-        className={classNames("KaizntreeInputCheckbox--label", {
-          "KaizntreeInputCheckbox--label-checked": checked,
-          "KaizntreeInputCheckbox--label-disabled": disabled,
-        })}
-      />
-      <input
-        id={inputId}
-        type="checkbox"
-        className="KaizntreeInputCheckbox--input"
-        checked={checked}
-        disabled={disabled}
-        onChange={() => onChange(!checked)}
-      />
-    </div>
-  )
+    const { current: inputId } = useRef(`KaizntreeInputCheckbox-${id}`)
+    // Bug 2 solved
+    return (
+        <div className="KaizntreeInputCheckbox--container" data-testid={inputId}>
+            <input
+                className={classNames("KaizntreeInputCheckbox--label", {
+                    "KaizntreeInputCheckbox--label-checked": checked,
+                    "KaizntreeInputCheckbox--label-disabled": disabled,
+                })}
+                id={inputId}
+                type="checkbox"
+                // className="KaizntreeInputCheckbox--input"
+                checked={checked}
+                disabled={disabled}
+                onChange={() => onChange(!checked)}
+            />
+        </div>
+    )
 }
